bd9d0dc57fb85d8b248e81e39efbb750eeb7f382
Hendrik Roth <hi@hendrikroth.com>
2021-02-01 15:15:34 +0100

    fix bug for fileparameters

--- junglejs/index.js
+++ junglejs/index.js
@@ -289,7 +289,8 @@ async function processFileForParameters(file, dirname, src, extension) {
         });
 
         data.forEach(d => {
-            const pFilename = fileParameters.map(k => d[k].split("-").map(s => s.charAt(0).toUpperCase() + s.slice(1))).join("");
+            const pFilename = fileParameters.map(k => d[k].split("-").map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(""));
+            console.log("PFILENAME",pFilename);
             let processedFile = rawSvelteFile;
 
             keys.forEach(k => {
